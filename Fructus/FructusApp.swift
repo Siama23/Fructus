57dab5927c743fe5a24bfe6f5b5aaa244cf442b2
Alessia Masola <alessiamasola23@gmail.com>
2022-06-29 13:34:25 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alessia Masola on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
